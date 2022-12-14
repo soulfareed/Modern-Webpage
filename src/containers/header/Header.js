e2673a21ebcbc400b9c84eb8fc67d7529ee6ddea
fareed <fareedzaman501@gmail.com>
2022-12-14 20:22:20 +0530

    add

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -7,8 +7,12 @@ const Header = () => {
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let’s Build Something amazing with GPT-3 OpenAI
-          <div className="gpt3__header-content__input">
-            <input type="email" placeholder="Your Email Adress" />
+          <div className="gpt3__header-content_input">
+            <input
+              className="email-input"
+              type="email"
+              placeholder="Your Email Adress"
+            />
             <button type="button">Get Started</button>
           </div>
         </h1>
