1dc235cc6cc13bf68593ffc03d548344ecbd005c
fareed <fareedzaman501@gmail.com>
2022-12-14 20:11:48 +0530

    add new codes

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+          <div className="gpt3__header-content__input">
+            <input type="email" placeholder="Your Email Adress" />
+            <button type="button">Get Started</button>
+          </div>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
