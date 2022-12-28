77810e4e6909f9e54263c0064f89ffd063de9d81
fareed <fareedzaman501@gmail.com>
2022-12-28 23:33:59 +0530

    ad

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -5,7 +5,7 @@ import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
-    <div className="gpt3__header section__padding" id="home">
+    < className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let’s Build Something amazing with GPT-3 OpenAI
@@ -28,7 +28,7 @@ const Header = () => {
           <img src={people} />
         </div>
       </div>
-    </div>
+    </
   );
 };
 
