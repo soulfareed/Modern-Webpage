09ec9c103d6bab4847b5ba3197736f500a8c96d7
fareed <fareedzaman501@gmail.com>
2023-01-03 21:46:04 +0530

    ad

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -36,9 +36,7 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
-        <div className="gpt3__navbar-links_container">
-          <Menu />
-        </div>
+        <div className="gpt3__navbar-links_container"></div>
       </div>
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
