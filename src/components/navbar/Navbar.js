e3eef5fe4f52bd4fcdb2b0659965719a922a5584
fareed <fareedzaman501@gmail.com>
2023-01-03 21:23:27 +0530

    ad

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -44,9 +44,9 @@ const Navbar = () => {
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
-      <div className="gpt3__navbar-menu">
-        {toggleMenu ? (
-          <RiCloseLine
+      <div classNamegpt3__navbar-menu">
+        {toggleMenu (
+          <
             color="#fff"
             size={27}
             onClick={() => setToggleMenu(false)}
