515dbd3afac0764abfa8d71cd6b85d861e774d5c
fareed <fareedzaman501@gmail.com>
2023-01-08 20:33:59 +0530

    ad

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -24,7 +24,9 @@ const Header = () => {
           />
           <button type="button">Get Started</button>
         </div>
-        <div className="gpt3__header-content__people"></div>
+        <div className="gpt3__header-content__people">
+          <img src={people} />
+        </div>
       </div>
     </div>
   );
