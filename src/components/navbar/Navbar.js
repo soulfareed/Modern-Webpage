139701344956091e60fb8342acd75f874f60b68e
fareed <fareedzaman501@gmail.com>
2023-03-29 22:52:13 +0530

    asdf

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -60,7 +60,6 @@ const Navbar = () => {
         )}
         {toggleMenu && (
           <div className="gpt3__navbar-menu_container scale-up-center">
-            <div className="gpt3__navbar-menu_container-links">
               <Menu />
               <div className="gpt3__navbar-menu_container-links-sign">
                 <p>Sign in</p>
